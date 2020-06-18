726e6994efe0e58990dc95bab4a9ffd32afd8952
chaoyang_chen <chaoyangc@gmail.com>
2020-06-18 09:08:45 +0000

    Added VPC module111111

--- main.tf
+++ main.tf
@@ -4,7 +4,8 @@ provider "aws" {
 }
 
 resource aws_vpc "hashicat" {
-  cidr_block           = var.address_space
+  cidr_block = "10.0.0.0/16"
+#  cidr_block           = var.address_space
   enable_dns_hostnames = true
 
   tags = {
@@ -30,7 +31,7 @@ resource aws_security_group "hashicat" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
