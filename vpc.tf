16dbfaccc95cab072f92c264afa36481f2677f93
chaoyang_chen <chaoyangc@gmail.com>
2020-06-18 08:57:59 +0000

    Added VPC module

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,8 @@
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "2.39.0"
-  # insert the 12 required variables here
-  cidr_block = "10.99.0.0/16"
 }
 
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
